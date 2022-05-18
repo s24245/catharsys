49da428b80e57710b311f4be2bbbb0b172027a50
vankokos <vano9995@gmail.com>
2022-05-18 14:11:21 +0200

    a

--- js/getMessage.js
+++ js/getMessage.js
@@ -16,65 +16,64 @@ let allThoughtsArr = api.get('/thoughts/')
 
             for (let i = 0; i < thoughts.length; i++) {
                 newArr.push(thoughts[i])
-                console.log(thoughts[i].text)
+                // console.log(thoughts[i].text)
 
-
-                let messagesInnerBlocks = document.querySelector(".messages__inner-blocks")
-                let messagesInnerBlocksBlock = document.createElement("div")
-                messagesInnerBlocksBlock.classList.add("messages__inner-blocks-block")
-                
-                let messagesUser = document.createElement("div")
-                messagesUser.classList.add("messages__inner-blocks-block-user")
-                let messageText = document.createElement("div")
-                messageText.classList.add("messages__inner-blocks-block-text")
-                let messageReactions = document.createElement("div")
-                messageReactions.classList.add("messages__inner-blocks-block-reactions")
-    
-    
-                let messageUserImg = document.createElement("img")
-                messageUserImg.src = "img/user.svg"
-    
-                let messageTextH = document.createElement("h1")
-                messageTextH.innerHTML = thoughts[i].text;
-
-                let messageReactionsHeart = document.createElement("div")
-                messageReactionsHeart.classList.add("messages__inner-blocks-block-reactions-heart")
-                let messageReactionsHeartH = document.createElement("h1")
-                messageReactionsHeartH.classList.add("messages__inner-blocks-block-reactions-heart-counter")
-                let messageReactionsHeartImg = document.createElement("img")
-                messageReactionsHeartImg.src = "img/heart.png"
-
-                let messageReactionsLike = document.createElement("div")
-                messageReactionsLike.classList.add("messages__inner-blocks-block-reactions-like")
-                let messageReactionsLikeH = document.createElement("h1")
-                messageReactionsLikeH.classList.add("messages__inner-blocks-block-reactions-like-counter")
-                let messageReactionsLikeImg = document.createElement("img")
-                messageReactionsLikeImg.src = "img/like.png"
-                
-
-
-                messagesInnerBlocks.appendChild(messagesInnerBlocksBlock)
-                messagesInnerBlocksBlock.appendChild(messagesUser)
-                messagesUser.appendChild(messageUserImg)
-
-                messagesInnerBlocksBlock.appendChild(messageText)
-                messageText.appendChild(messageTextH)
-
-                messagesInnerBlocksBlock.appendChild(messageReactions)
-                messageReactions.appendChild(messageReactionsHeart)
-                messageReactionsHeart.appendChild(messageReactionsHeartImg)
-                messageReactionsHeart.appendChild(messageReactionsHeartH)
-
-                messagesInnerBlocksBlock.appendChild(messageReactions)
-                messageReactions.appendChild(messageReactionsLike)
-                messageReactionsLike.appendChild(messageReactionsLikeImg)
-                messageReactionsLike.appendChild(messageReactionsLikeH)
 
             }
+            
+            console.log(thought.text)
+            
+            let messagesInnerBlocks = document.querySelector(".messages__inner-blocks")
+            let messagesInnerBlocksBlock = document.createElement("div")
+            messagesInnerBlocksBlock.classList.add("messages__inner-blocks-block")
+            
+            let messagesUser = document.createElement("div")
+            messagesUser.classList.add("messages__inner-blocks-block-user")
+            let messageText = document.createElement("div")
+            messageText.classList.add("messages__inner-blocks-block-text")
+            let messageReactions = document.createElement("div")
+            messageReactions.classList.add("messages__inner-blocks-block-reactions")
 
 
+            let messageUserImg = document.createElement("img")
+            messageUserImg.src = "img/user.svg"
+
+            let messageTextH = document.createElement("h1")
+            messageTextH.innerHTML = thought.text
+
+            let messageReactionsHeart = document.createElement("div")
+            messageReactionsHeart.classList.add("messages__inner-blocks-block-reactions-heart")
+            let messageReactionsHeartH = document.createElement("h1")
+            messageReactionsHeartH.classList.add("messages__inner-blocks-block-reactions-heart-counter")
+            let messageReactionsHeartImg = document.createElement("img")
+            messageReactionsHeartImg.src = "img/heart.png"
+
+            let messageReactionsLike = document.createElement("div")
+            messageReactionsLike.classList.add("messages__inner-blocks-block-reactions-like")
+            let messageReactionsLikeH = document.createElement("h1")
+            messageReactionsLikeH.classList.add("messages__inner-blocks-block-reactions-like-counter")
+            let messageReactionsLikeImg = document.createElement("img")
+            messageReactionsLikeImg.src = "img/like.png"
+            
 
 
+            messagesInnerBlocks.appendChild(messagesInnerBlocksBlock)
+            messagesInnerBlocksBlock.appendChild(messagesUser)
+            messagesUser.appendChild(messageUserImg)
+
+            messagesInnerBlocksBlock.appendChild(messageText)
+            messageText.appendChild(messageTextH)
+
+            messagesInnerBlocksBlock.appendChild(messageReactions)
+            messageReactions.appendChild(messageReactionsHeart)
+            messageReactionsHeart.appendChild(messageReactionsHeartImg)
+            messageReactionsHeart.appendChild(messageReactionsHeartH)
+
+            messagesInnerBlocksBlock.appendChild(messageReactions)
+            messageReactions.appendChild(messageReactionsLike)
+            messageReactionsLike.appendChild(messageReactionsLikeImg)
+            messageReactionsLike.appendChild(messageReactionsLikeH)
+            
         }
     }
 })
