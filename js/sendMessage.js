c2da834ff1b4749eaabd76950a9123594411f73a
F4 Developer <skowanski@f4dev.me>
2022-05-21 19:39:58 +0200

    Added code validation on page load; reversed messages order in messages.html

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -1,35 +1,54 @@
-let messageStranger = document.querySelector("#messageStranger")
-let submitStranger = document.querySelector("#submitStranger")
-let testNumStranger = document.querySelector("#testNumStranger")
-
-console.log(testNumStranger)
+let messageStranger = document.querySelector("#messageStranger"),
+    submitStranger  = document.querySelector("#submitStranger"),
+    shareCode;
 
 const api = axios.create({
-    baseURL: 'https://catharsys.staging.f4dev.me/'
-})
+                           baseURL: "https://catharsys.staging.f4dev.me/"
+                         });
 
-// console.log(api)    
+submitStranger.addEventListener("click", (e) => {
+  e.preventDefault();
 
+  api.post("/thoughts/", {
+    code:      shareCode,
+    published: true,
+    text:      messageStranger.value
+  })
+     .then((response) => {
+       if(!response.data.error) {
+         location.replace("messages.html");
+       }
+     })
+     .catch((error) => {
+       console.error(error);
+     });
 
-submitStranger.addEventListener("click", (e)=> {
-    e.preventDefault()
+});
 
-    api.post('/thoughts/', {
-        code: +testNumStranger.value,
-        published: true,
-        text: messageStranger.value
-    })
-        .then((response) => {
-            if(!response.data.error) {
-                const thought = response.data.data
-    
-                console.log(`Thought with text "${thought.text}" created successfully`)
+function validateCode() {
+  const hash     = new URLSearchParams(location.hash.substring(1)),
+        code     = hash.get("code"),
+        redirect = () => {
+          location.replace("messages.html"); // TODO: replace to the page with random entries + some message
+        };
 
-                location.replace("messages.html")
-            }
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+  if(!code) {
+    return redirect();
+  }
 
-})
+  api.get(`/codes/check/${code}`)
+     .then(res => {
+       const code = res.data;
+
+       if(res.status !== 200 || code.error || code.data.used) {
+         return redirect();
+       } else {
+         shareCode = code.data.id;
+       }
+     })
+     .catch(() => {
+       return redirect();
+     });
+}
+
+validateCode();
